665706b968147dd99a26583f886237394a07bf97
tarunChand28298 <44527962+tarunChand28298@users.noreply.github.com>
2019-04-07 12:24:17 +0530

    Functioning Input and output to the shader

--- Main.cpp
+++ Main.cpp
@@ -1,5 +1,3 @@
-//problem at line 126 because of line 119.
-
 #include <Windows.h>
 #include <d3d11.h>
 #include <d3dcompiler.h>
@@ -11,7 +9,10 @@ struct Sphere {
 	float specularx, speculary, specularz;
 };
 Sphere spheres[] = {
-	{0.5f, 0.1f, 0.8f, 1.0f}
+	{0.0f, 0.0f, 0.0f, 2.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f},
+	{3.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f},
+	{0.0f, 3.0f, 0.0f, 1.0f, 0.0f, 3.0f, 0.0f, 0.0f, 0.0f, 0.0f},
+	{0.0f, 0.0f, 3.0f, 1.0f, 0.0f, 0.0f, 3.0f, 0.0f, 0.0f, 0.0f}
 };
 
 #pragma region Global Variables:
@@ -24,17 +25,17 @@ int WindowHeight = 600;
 LRESULT CALLBACK DirectXWindowProc(HWND windowHanlde, UINT message, WPARAM wparam, LPARAM lparam) {
 
 	switch (message) {
-		case WM_QUIT: {
-			DestroyWindow(windowHanlde);
-			break;
-		}
-		case WM_DESTROY: {
-			running = false;
-			break;
-		}
-		default: {
-			break;
-		}
+	case WM_QUIT: {
+		DestroyWindow(windowHanlde);
+		break;
+	}
+	case WM_DESTROY: {
+		running = false;
+		break;
+	}
+	default: {
+		break;
+	}
 	}
 
 	return DefWindowProc(windowHanlde, message, wparam, lparam);
@@ -43,23 +44,23 @@ LRESULT CALLBACK DirectXWindowProc(HWND windowHanlde, UINT message, WPARAM wpara
 int WINAPI WinMain(HINSTANCE instance, HINSTANCE prevInstance, char* cmdArgs, int cmdShow) {
 	//=============================================================================================================================================
 
-	#pragma region Stack Variables:
+#pragma region Stack Variables:
 	HWND windowHandle;
 
-	ID3D11Device*					device = nullptr;
+	ID3D11Device*				device = nullptr;
 	ID3D11DeviceContext*			deviceContext = nullptr;
-	IDXGISwapChain*					swapchain = nullptr;
+	IDXGISwapChain*				swapchain = nullptr;
 
 	ID3D11ComputeShader*			computeShader = nullptr;
 
-	ID3D11Buffer*					inputSphereBuffer = nullptr;
+	ID3D11Buffer*				inputSphereBuffer = nullptr;
 	ID3D11ShaderResourceView*		inputSphereBufferView = nullptr;
 
 	ID3D11UnorderedAccessView*		outputBackBuffer = nullptr;
-	#pragma endregion
+#pragma endregion
 
 	//=============================================================================================================================================
-	#pragma region Create Window:
+#pragma region Create Window:
 	{
 		WNDCLASS wc = {};
 		wc.hInstance = instance;
@@ -72,21 +73,21 @@ int WINAPI WinMain(HINSTANCE instance, HINSTANCE prevInstance, char* cmdArgs, in
 		windowHandle = CreateWindow("DirectXWindowClass", "DirectX Render Window", WS_OVERLAPPED | WS_SYSMENU | WS_MINIMIZEBOX, CW_USEDEFAULT, CW_USEDEFAULT, WindowWidth, WindowHeight, nullptr, nullptr, instance, nullptr);
 		ShowWindow(windowHandle, SW_SHOW);
 	}
-	#pragma endregion
-	#pragma region Create Device and SwapChain:
+#pragma endregion
+#pragma region Create Device and SwapChain:
 	{
 		DXGI_SWAP_CHAIN_DESC scd = {};
 		scd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
 		scd.SampleDesc.Count = 1;
-		scd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT | DXGI_USAGE_UNORDERED_ACCESS;
+		scd.BufferUsage = DXGI_USAGE_UNORDERED_ACCESS;
 		scd.BufferCount = 2;
 		scd.OutputWindow = windowHandle;
 		scd.Windowed = true;
-
+		//in the end, change creation flag from D3D11_CREATE_DEVICE_DEBUG to 0.
 		D3D11CreateDeviceAndSwapChain(nullptr, D3D_DRIVER_TYPE_HARDWARE, nullptr, D3D11_CREATE_DEVICE_DEBUG, nullptr, 0, D3D11_SDK_VERSION, &scd, &swapchain, &device, nullptr, &deviceContext);
 	}
-	#pragma endregion
-	#pragma region Create the Shader:
+#pragma endregion
+#pragma region Create the Shader:
 	{
 		ID3D10Blob* shaderData;
 		D3DReadFileToBlob((LPCWSTR)L"ComputeShader.cso", &shaderData);
@@ -96,25 +97,26 @@ int WINAPI WinMain(HINSTANCE instance, HINSTANCE prevInstance, char* cmdArgs, in
 
 		shaderData->Release();
 	}
-	#pragma endregion
-	#pragma region Create Input for Shader:
+#pragma endregion
+#pragma region Create Input for Shader:
 	{
-		//Create input buffer:
+		//Create input buffer (Spheres):
 		D3D11_BUFFER_DESC bd = {};
 		bd.ByteWidth = sizeof(Sphere)*ARRAYSIZE(spheres);
 		bd.Usage = D3D11_USAGE_DEFAULT;
 		bd.BindFlags = D3D11_BIND_SHADER_RESOURCE;
 		bd.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
 		bd.StructureByteStride = sizeof(Sphere);
+		bd.MiscFlags = D3D11_RESOURCE_MISC_BUFFER_STRUCTURED;
 
 		D3D11_SUBRESOURCE_DATA srd = {};
 		srd.pSysMem = spheres;
 		srd.SysMemPitch = 0;
 		srd.SysMemSlicePitch = 0;
-		
+
 		device->CreateBuffer(&bd, &srd, &inputSphereBuffer);
 
-		//create the SRV:
+		//create the SRV (Spheres):
 		D3D11_SHADER_RESOURCE_VIEW_DESC srvd = {};
 		srvd.Format = DXGI_FORMAT_UNKNOWN;
 		srvd.ViewDimension = D3D11_SRV_DIMENSION_BUFFER;
@@ -123,16 +125,13 @@ int WINAPI WinMain(HINSTANCE instance, HINSTANCE prevInstance, char* cmdArgs, in
 		srvd.Buffer.FirstElement = 0;
 		srvd.Buffer.NumElements = ARRAYSIZE(spheres);
 
-		HRESULT hr = device->CreateShaderResourceView(inputSphereBuffer, &srvd, &inputSphereBufferView); //I get the following error:
-		//ID3D11Device::CreateShaderResourceView: The Format (0, UNKNOWN) cannot be used, when creating a View of a Buffer.
-		//My problem is, none of the dxgi formats match up with the format I want.
-		//I need 3 float vectors and one float. There is nothing like it in the dxgi format.
-		
-		//Set the SRV:
+		device->CreateShaderResourceView(inputSphereBuffer, &srvd, &inputSphereBufferView);
+
+		//Set the SRV (Spheres):
 		deviceContext->CSSetShaderResources(0, 1, &inputSphereBufferView);
 	}
-	#pragma endregion
-	#pragma region Create Output for Shader:
+#pragma endregion
+#pragma region Create Output for Shader:
 	{
 		//Get the backbuffer from the swap chain:
 		ID3D11Texture2D* backBuffer = nullptr;
@@ -156,13 +155,13 @@ int WINAPI WinMain(HINSTANCE instance, HINSTANCE prevInstance, char* cmdArgs, in
 
 		backBuffer->Release();
 	}
-	#pragma endregion
+#pragma endregion
 
 	//=============================================================================================================================================
 
 	running = true;
 	while (running) {
-		#pragma region Handle Window Messages:
+#pragma region Handle Window Messages:
 		{
 			MSG message = {};
 			while (PeekMessage(&message, nullptr, 0, 0, PM_REMOVE)) {
@@ -175,36 +174,36 @@ int WINAPI WinMain(HINSTANCE instance, HINSTANCE prevInstance, char* cmdArgs, in
 				}
 			}
 		}
-		#pragma endregion
-		#pragma region Handle Input:
+#pragma endregion
+#pragma region Handle Input:
 		{
 
 		}
-		#pragma endregion
-		#pragma region Update Frame:
+#pragma endregion
+#pragma region Update Frame:
 		{
 
 		}
-		#pragma endregion
-		#pragma region Render to Screen:
+#pragma endregion
+#pragma region Render to Screen:
 		{
-			deviceContext->Dispatch(WindowWidth/8, WindowHeight/8, 1);
+			deviceContext->Dispatch(WindowWidth / 8, WindowHeight / 8, 1);
 			swapchain->Present(1, 0);
 		}
-		#pragma endregion
+#pragma endregion
 	}
 
 	//=============================================================================================================================================
 
-	#pragma region Clean Up:
+#pragma region Clean Up:
 	device->Release();
 	deviceContext->Release();
 	swapchain->Release();
 	computeShader->Release();
 	inputSphereBuffer->Release();
-	//inputBufferView->Release();
+	inputSphereBufferView->Release();
 	outputBackBuffer->Release();
-	#pragma endregion
+#pragma endregion
 
 	return 0;
 }
